a763925bc575659c3f9bae8df3193a1ddc1c22ad
Dima Levchuk <levchukua3@gmail.com>
2019-02-26 17:44:57 +0200

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://clap-clap/admin/');
+define('HTTP_CATALOG', 'http://clap-clap/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://clap-clap/admin/');
+define('HTTPS_CATALOG', 'http://clap-clap/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/Programs/OSPanel/domains/clap-clap/admin/');
+define('DIR_SYSTEM', 'E:/Programs/OSPanel/domains/clap-clap/system/');
+define('DIR_IMAGE', 'E:/Programs/OSPanel/domains/clap-clap/image/');
+define('DIR_LANGUAGE', 'E:/Programs/OSPanel/domains/clap-clap/admin/language/');
+define('DIR_TEMPLATE', 'E:/Programs/OSPanel/domains/clap-clap/admin/view/template/');
+define('DIR_CONFIG', 'E:/Programs/OSPanel/domains/clap-clap/system/config/');
+define('DIR_CACHE', 'E:/Programs/OSPanel/domains/clap-clap/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/Programs/OSPanel/domains/clap-clap/system/storage/download/');
+define('DIR_LOGS', 'E:/Programs/OSPanel/domains/clap-clap/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/Programs/OSPanel/domains/clap-clap/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/Programs/OSPanel/domains/clap-clap/system/storage/upload/');
+define('DIR_CATALOG', 'E:/Programs/OSPanel/domains/clap-clap/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'site_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://clap-clap/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://clap-clap/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/Programs/OSPanel/domains/clap-clap/catalog/');
+define('DIR_SYSTEM', 'E:/Programs/OSPanel/domains/clap-clap/system/');
+define('DIR_IMAGE', 'E:/Programs/OSPanel/domains/clap-clap/image/');
+define('DIR_LANGUAGE', 'E:/Programs/OSPanel/domains/clap-clap/catalog/language/');
+define('DIR_TEMPLATE', 'E:/Programs/OSPanel/domains/clap-clap/catalog/view/theme/');
+define('DIR_CONFIG', 'E:/Programs/OSPanel/domains/clap-clap/system/config/');
+define('DIR_CACHE', 'E:/Programs/OSPanel/domains/clap-clap/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/Programs/OSPanel/domains/clap-clap/system/storage/download/');
+define('DIR_LOGS', 'E:/Programs/OSPanel/domains/clap-clap/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/Programs/OSPanel/domains/clap-clap/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/Programs/OSPanel/domains/clap-clap/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'site_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
